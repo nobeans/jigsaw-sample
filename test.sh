7c437b9178e437aace37efa81e8e85ea61c55af9
Yasuharu Nakano <ynak@jggug.org>
2017-10-10 15:59:44 +0900

    Run executable jar

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -xe
 
 find . -name "*.class" | xargs rm
+find mods -name "*.jar" | xargs rm
 
 find . -name "*.java" | xargs javac
 
 java -cp src/main/java my.HelloWorld
 java -cp src/main/java my.HelloWorld FooBar
+
+mkdir -p mods
+jar --create --file mods/my-app.jar --main-class my.HelloWorld -C src/main/java .
+
+java -jar my-app.jar
+java -jar my-app.jar FooBar
